97e51191022dd1322ea128ee4b0a6906b4e53f76
jorgesolis1989 <jorgesolis1989@gmail.com>
2016-03-14 18:40:13 -0500

    Acomodando el modelo sin clase persona ni rol

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import Permission
 from django.views.decorators.csrf import requires_csrf_token
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -14,31 +15,34 @@ from django.template.context import RequestContext
 from django.core.mail import EmailMessage
 
 from django.shortcuts import render
-from usuarios.models import USUARIO , PERSONA , ROL
+from usuarios.models import Usuario
 from usuarios.forms import FormularioLogin
 from usuarios.forms import FormularioRegistroUsuario
-
+from usuarios.backends import BackendUsuarios
 
 #Método auxiliar para encontrar la vista del usuario
 def retornar_vista(request , usuario):
-    if usuario.rol.nombre == "Administrador":
+    print(usuario.get_all_permissions())
+    if usuario.has_perm("usuarios.Administrador"):
         return administrador_home(request, usuario)
-    elif usuario.rol.nombre == "Votante":
+    elif usuario.has_perm("usuarios.Votante"):
         return votante_home(request, usuario)
-    else:
+    elif usuario.has_perm("usuarios.Superior"):
         return superior_home(request, usuario)
-
+    else:
+        return login_view()
 # Pagina principal para usuario Administrador
-@login_required()
+@permission_required("usuarios.Administrador" , login_url="/")
 def administrador_home(request , usuario):
     return render(request, 'administrador.html', {'usuario': usuario})
 
-
 # Pagina del home  para usuario Votante
+@permission_required("usuarios.Votante" , login_url="/")
 def votante_home(request, usuario):
     return render(request , 'votante.html', {'usuario': usuario})
 
 # Pagina del home  para usuario Superior
+@permission_required("usuarios.Superior" , login_url="/")
 def superior_home(request, usuario):
     return render(request , 'superior.html', {'usuario': usuario})
 
@@ -47,18 +51,18 @@ def superior_home(request, usuario):
 def login_view(request):
     mensaje = ""
     if request.user.is_authenticated() and not request.user.is_superuser:
-        usuario = USUARIO.objects.get(persona__cedula=request.user.username)
+        usuario = Usuario.objects.get(username=request.user.username)
         return retornar_vista(request, usuario)
 
     elif request.method == 'POST':
         form = FormularioLogin(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            acceso = authenticate(username=cd['username'], password=cd['password'])
-            if acceso is not None:
-                if acceso.is_active:
-                    login(request, acceso)
-                    usuario = USUARIO.objects.get(persona__cedula=cd['username'])
+            usuario = authenticate(username=cd['username'], password=cd['password'])
+            if usuario is not None:
+                if usuario.is_active:
+                    login(request, usuario)
+
                     #Redireccionar
                     return retornar_vista(request, usuario)
                 else:
@@ -70,54 +74,50 @@ def login_view(request):
     return render(request, 'login.html', {'mensaje': mensaje, 'form': form })
 
 #Vista de registro de usuarios
-@login_required()
+@permission_required("usuarios.Administrador" , login_url="/")
 def registro_usuario(request):
     mensaje = ""
     if request.method == 'POST':
         form = FormularioRegistroUsuario(request.POST)
+
+        #Si el formulario es valido y tiene datos, limpielo
         if form.is_valid():
+            #Capture la cedula del usuario
             cedula_usuario = form.cleaned_data["cedula_usuario"]
-            persona = PERSONA.objects.filter(cedula=cedula_usuario)
-            if not persona:
-                nombre_usuario = form.cleaned_data["nombre_usuario"]
-                apellido_usuario = form.cleaned_data["apellido_usuario"]
-                correo = form.cleaned_data["email"]
-                rol = form.cleaned_data["rol"]
 
-                # Creando la persona.
-                persona = PERSONA()
-                persona.cedula = cedula_usuario
-                persona.nombre = nombre_usuario
-                persona.apellidos = apellido_usuario
-                persona.correo = correo
+            #Consultando el usuario en la base de datos.
+            usuario = Usuario.objects.filter(cedula_usuario=cedula_usuario)
 
-                try:
-                    persona.save()
-                except Exception as e:
-                    print(e)
-
+            #Si el usuario no existe, lo crea
+            if not usuario:
                 # Creando el usuario
-                usuario = USUARIO()
+                usuario = Usuario()
+                usuario.cedula_usuario = cedula_usuario
+                usuario.first_name = form.cleaned_data["nombre_usuario"]
+                usuario.last_name = form.cleaned_data["apellido_usuario"]
+                usuario.email = form.cleaned_data["email"]
                 usuario.username = cedula_usuario
-                usuario.persona = PERSONA.objects.get(cedula=cedula_usuario)
-                usuario.rol = ROL.objects.get(nombre=rol)
+                #generando el password aleatorio.
                 password = User.objects.make_random_password()
-                print("El password es : "+password)
                 usuario.set_password(password)
-                usuario.observacion =  "Creando USUARIO " + str(cedula_usuario)
+
+                #usuario.user_permissions.add(form.cleaned_data["rol"])
+                print(password)
 
                 # Enviando contraseña al correo electronico registrado.
-                mensaje = "Señor(a) ", usuario.persona.nombre , "\nSu usuario de acceso es: ", usuario.username , "\n Contraseña: ", password
-                send_mail('Envío de contraseña de acceso', mensaje, 'sivoreunivalle@gmail.com', [correo], fail_silently=False)
+                mensaje = "Señor(a) ", usuario.first_name , "\nSu usuario de acceso es: ", usuario.cedula_usuario , "\n Contraseña: ", usuario.password
+                #send_mail('Envío de contraseña de acceso a SIVORE', mensaje, 'sivoreunivalle@gmail.com', [usuario.email], fail_silently=False)
 
+                #Crea el usuario en la BD s i hay excepcion
                 try:
                     usuario.save()
                 except Exception as e:
                     print(e)
 
+                usuario.user_permissions.add(Permission.objects.get(codename=form.cleaned_data["rol"]))
 
                 form = FormularioRegistroUsuario()
-                mensaje = "El usuario se guardo correctamente, la contraseña se envío al correo " + correo
+                mensaje = "El usuario se guardo correctamente, la contraseña se envío al correo " + usuario.email
             else:
                 form = FormularioRegistroUsuario()
                 mensaje = "El usuario " + str(cedula_usuario)  + " ya esta registrado"
@@ -135,20 +135,20 @@ def registro_usuario(request):
     return render(request, 'registro_usuario.html',{'mensaje': mensaje, 'form': form})
 
 # Vista para listar usuarios
-@login_required()
+@permission_required("usuarios.Administrador" , login_url="/")
 def listar_usuario(request):
-    usuarios = USUARIO.objects.all()
+    usuarios = Usuario.objects.all()
     return render(request, 'listar.html', {'usuarios': usuarios})
 
-#Registro usuarios
-@login_required()
+#Edicion usuarios
+@permission_required("usuarios.Administrador" , login_url="/")
 def editar_usuario(request, username=None):
     print(username)
     if username is None:
         return render(request, 'administrador.html')
     else:
-        usuario = USUARIO.objects.get(username=username)
+        usuario = Usuario.objects.get(username=username)
         form = FormularioRegistroUsuario()
-        form.initial = {'cedula_usuario': usuario.username, 'nombre_usuario': usuario.persona.nombre , 'apellido_usuario': usuario.persona.apellidos ,
-                        'rol': usuario.rol.nombre , 'email': usuario.persona.correo }
+        form.initial = {'cedula_usuario': usuario.cedula_usuario, 'nombre_usuario': usuario.first_name , 'apellido_usuario': usuario.last_name ,
+                        'rol': "Administrador" , 'email': usuario.email }
     return render(request, 'editar_usuario.html', {'form': form})
