605eeff769e584e6a273492b1ea31415a503ae67
martha <martha>
2016-04-18 22:54:01 -0500

    registro jornada en views.py

--- jornadas/forms.py
+++ jornadas/forms.py
@@ -7,16 +7,16 @@ Este formulario se encuentran los datos para registrar una jornada
 class FormularioRegistroJornada(forms.Form):
 
     nombre_jornada = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escriba aquí el nombre del usuario', 'required':'true', 'data-width':'100%'}))
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Escriba aquí el nombre de la jornada', 'required':'true', 'data-width':'100%'}))
 
+    fecha_jornada = forms.DateField(input_formats='%d/%m/%y',widget=forms.DateInput(attrs={'type':'text', 'class':'form-control', 'required':'true'}))
 
+    hora_inicio = forms.TimeField(input_formats='%H:%M %p', widget=forms.TimeInput(attrs={'id':'demo-tp-com', 'type':'text', 'class':'form-control', 'required':'true'}))
 
-    fecha_jornada = forms.DateField()
+    hora_final = forms.TimeField(input_formats='%H:%M %p', widget=forms.TimeInput(attrs={'id':'demo-tp-com-1', 'type':'text', 'class':'form-control', 'required':'true'}))
 
-    hora_inicio = forms.DateTimeInput()
-
-    hora_final = forms.DateTimeInput()
-
-    corporaciones = forms.ModelChoiceField(widget=forms.Select(attrs={'multiple tabindex':'4', 'data-placeholder':'Escoger las corporaciones'}), queryset=Corporacion.objects.all(), required=True, empty_label=None)
+    corporaciones = forms.ModelChoiceField(widget=forms.Select(attrs={'id':'demo-cs-multiselect', 'data-live-search':'true',
+                                                                      'multiple tabindex':'4', 'data-placeholder':'Escoger las corporaciones'}),
+                                           queryset=Corporacion.objects.all(), required=True, empty_label=None)
 
 
--- jornadas/views.py
+++ jornadas/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render_to_response, render, redirect
 from jornadas.forms import FormularioRegistroJornada
+
 from jornadas.models import Jornada , Jornada_Corporacion
 
 
@@ -8,8 +9,43 @@ from jornadas.models import Jornada , Jornada_Corporacion
 @permission_required("usuarios.Administrador", login_url="/")
 def registro_jornada(request):
     #Verificación para crear una jornada
-    form = FormularioRegistroJornada()
-    return render(request, 'registro_jornada.html', {'form': form})
+    if request.method == 'POST' and  "btncreate" in request.POST:
+        form = FormularioRegistroJornada(request.POST)
+        #Si el formulario es valido
+        print(form)
+        if form.is_valid():
+            print("form jornada valido")
+            jornada = Jornada()
+            jornada.nombre_jornada = form.cleaned_data["nombre_jornada"]
+            jornada.fecha_inicio_jornada = form.cleaned_data["fecha_inicial"]
+            jornada.fecha_final_jornada = form.cleaned_data["fecha_final"]
+            try:
+                jornada.save()
+            except Exception as e:
+                print(e)
+
+            jornada_corporaciones = form.cleaned_data["corporaciones"]
+            for corporacion in jornada_corporaciones:
+                jornada_corporacion = Jornada_Corporacion()
+                #guardamos jornada
+                jornada_corporacion.jornada = jornada
+                #guardamos corporacion
+                jornada_corporacion.corporacion = corporacion
+                try:
+                    jornada_corporacion.save()
+                except Exception as e:
+                    print(e)
+        #si no es valido el formulario, crear
+        else:
+            mensaje = "Datos incompleto para crear la jornada"
+            llamarMensaje = "fracaso_usuario"
+            form = FormularioRegistroJornada()
+            data = {'mensaje': mensaje, 'form': form, 'llamarMensaje':llamarMensaje}
+            return render(request, 'registro_jornada.html', data)
+
+    else:
+        form = FormularioRegistroJornada()
+        return render(request, 'registro_jornada.html', {'form': form})
 
 # Vista para listar votantes
 @permission_required("usuarios.Administrador", login_url="/")
