08f8e9a4c18596e70d910fa0e877bbbc7ed0dbb7
jorgesolis1989 <jorgesolis1989@gmail.com>
2016-04-30 12:40:53 -0500

    Interfaz votantes - corporaciones dinamicas

--- votaciones/views.py
+++ votaciones/views.py
@@ -1,9 +1,34 @@
 from django.contrib.auth.decorators import permission_required
+from votantes.models import Votante
+from django.db.models import Q
+from jornadas.models import Jornada_Corporacion
+from corporaciones.models import Corporacion
+from datetime import datetime
 from django.shortcuts import render_to_response, render, redirect
 
 def mostrar_tarjeton(request):
+
     return render(request, 'mostrar_tarjeton.html')
 
 def mostrar_corporaciones(request):
-    return render(request, 'mostrar_corporaciones.html')
+    votantes_asociados = Votante.objects.filter(usuario__cedula_usuario=request.user.username)
 
+    # Corporaciones activas de la jornada
+    corporaciones_activas_jornada = Jornada_Corporacion.objects.filter(Q(jornada__fecha_inicio_jornada__lte = datetime.now()) ,
+                                                              jornada__is_active=True,
+                                                              jornada__fecha_final_jornada__gte = datetime.now())
+
+    print(corporaciones_activas_jornada)
+    # Corporaciones que estan permitidas por el usuario
+    corporaciones = []
+    for votante in votantes_asociados:
+            print(votante.plan.id_corporation  , votante.plan.facultad.id_corporation)
+            corporaciones += (Corporacion.objects.filter(Q(id_corporation=votante.plan.facultad.id_corporation) |
+                                     Q(id_corporation=votante.plan.id_corporation)).values_list("id_corporation" , flat=True))
+
+    corporaciones_activas_jornada = corporaciones_activas_jornada.filter(
+            Q(corporacion__id_corporation=1) |
+            Q(corporacion__id_corporation=2) |
+            Q(corporacion__id_corporation__in=corporaciones)).order_by("corporacion__id_corporation")
+    print(corporaciones_activas_jornada)
+    return render(request, 'mostrar_corporaciones.html' , {'corporaciones_activas':corporaciones_activas_jornada})
